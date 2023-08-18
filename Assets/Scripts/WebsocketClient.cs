c9fb1402b98a1a704721fdf008f57f5e8df1ac69
KingoBoiii <kingodk@outlook.dk>
2023-08-18 12:44:53 +0200

    Added some code that does something now one knows?

--- Assets/Scripts/WebsocketClient.cs
+++ Assets/Scripts/WebsocketClient.cs
@@ -4,6 +4,9 @@ using CMG.BallMazeGame.Models;
 using UnityEngine;
 using WebSocketSharp;
 using Newtonsoft.Json;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
 
 public class WebsocketClient : MonoBehaviour
 {
@@ -12,6 +15,25 @@ public class WebsocketClient : MonoBehaviour
 
     private void Start()
     {
+        // Create a UDP client
+        UdpClient udpClient = new UdpClient(12345);
+
+        // Enable broadcasting
+        udpClient.EnableBroadcast = true;
+
+        // Receive the broadcasted message
+        IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+        //byte[] receivedBytes = udpClient.Receive(ref remoteEndPoint);
+        //string message = Encoding.UTF8.GetString(receivedBytes);
+
+        // Display the received message
+        //Debug.Log("Received message: " + message);
+
+        // ws://192.168.109.173:8765
+
+        //_host = $"ws://{message}:8765";
+
         _ws = new WebSocket(_host);
         _ws.OnMessage += WsOnOnMessage;
         _ws.Connect();
